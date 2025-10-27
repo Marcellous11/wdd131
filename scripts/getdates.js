a2caac590d9cc6310e9c6f14b18194bb46ab1494
Marcellous Curtis Jr <1mcj001@gmail.com>
2025-10-27 14:38:58 -0500

    activity 1

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const today = new Date()
+
+const currentYear = document.getElementById("currentYear")
+
+currentYear.innerHTML = today.getFullYear()
+
